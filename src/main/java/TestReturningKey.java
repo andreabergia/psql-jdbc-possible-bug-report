3163c0b501f1ea53699352dd33bad1d09a103179
Andrea Bergia <a.bergia@list-group.com>
2016-07-15 16:08:00 +0200

    Fixed indentation

--- src/main/java/TestReturningKey.java
+++ src/main/java/TestReturningKey.java
@@ -6,20 +6,20 @@ import java.sql.SQLException;
 
 public class TestReturningKey {
     public static void main(String[] args) throws SQLException {
-try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost/sampledb", "sampledb", "")) {
-    connection.setAutoCommit(false);
-    try (PreparedStatement ps = connection.prepareStatement("INSERT INTO Documents (name) VALUES (?)", new String[]{"ID"})) {
-        ps.setString(1, "DocName");
-        ps.executeUpdate();
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost/sampledb", "sampledb", "")) {
+            connection.setAutoCommit(false);
+            try (PreparedStatement ps = connection.prepareStatement("INSERT INTO Documents (name) VALUES (?)", new String[]{"ID"})) {
+                ps.setString(1, "DocName");
+                ps.executeUpdate();
 
-        try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
-            if (!generatedKeys.next()) {
-                throw new RuntimeException("Should have been able to retrieve the generated keys");
+                try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
+                    if (!generatedKeys.next()) {
+                        throw new RuntimeException("Should have been able to retrieve the generated keys");
+                    }
+                    int generatedKey = generatedKeys.getInt(1);
+                    System.out.println("The generated key is " + generatedKey);
+                }
             }
-            int generatedKey = generatedKeys.getInt(1);
-            System.out.println("The generated key is " + generatedKey);
         }
-    }
-}
     }
 }
